1a5df14fe1dcf103902122aa972487cf4e435841
akshata-sirsikar05 <145318007+akshata-sirsikar05@users.noreply.github.com>
2023-09-18 12:31:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('breakfast menu')
-streamlit.text('hello')
-streamlit.text('bye')
-streamlit.text('bye')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
